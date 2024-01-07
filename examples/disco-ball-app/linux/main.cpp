db733dc62801b54f7603f735be96ee3aae0dd6cd
Cecille Freeman <cecille@google.com>
2024-01-07 17:21:20 -0500

    Run support for disco ball on pi

--- examples/disco-ball-app/linux/main.cpp
+++ examples/disco-ball-app/linux/main.cpp
@@ -18,6 +18,10 @@
 
 #include <AppMain.h>
 
+#if PI_DISCO_BALL
+#include <pigpiod_if2.h>
+#endif
+
 #include <memory>
 #include <utility>
 
@@ -48,6 +52,9 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
 {
   public:
     SampleDiscoBallDriver() = default;
+    ~SampleDiscoBallDriver() {
+        Shutdown();
+    }
 
     // Implementation of chip::app::DiscoBallClusterLogic::DriverInterface
     DiscoBallCapabilities GetCapabilities(EndpointId endpoint_id) const override
@@ -56,11 +63,7 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
 
         DiscoBallCapabilities capabilities;
 
-        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kAxis);
-        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kWobble);
         capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kReverse);
-        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kPattern);
-        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kStatistics);
 
         capabilities.min_speed_value = 0;
         capabilities.max_speed_value = 200;
@@ -71,9 +74,16 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
         capabilities.min_wobble_speed_value = 0;
         capabilities.max_wobble_speed_value = 100;
 
+#if !PI_DISCO_BALL
+        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kAxis);
+        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kWobble);
+        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kPattern);
+        capabilities.supported_features.Set(Clusters::DiscoBall::Feature::kStatistics);
+
         capabilities.wobble_support.Set(Clusters::DiscoBall::WobbleBitmap::kWobbleLeftRight);
         capabilities.wobble_support.Set(Clusters::DiscoBall::WobbleBitmap::kWobbleUpDown);
         capabilities.wobble_support.Set(Clusters::DiscoBall::WobbleBitmap::kWobbleRound);
+#endif
 
         return capabilities;
     }
@@ -81,20 +91,24 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
     Status OnClusterStateChange(EndpointId endpoint_id, BitFlags<DiscoBallFunction> changes, DiscoBallClusterLogic & cluster)
     {
         ChipLogDetail(Zcl, "DiscoBall endpoint %u state change", static_cast<unsigned>(endpoint_id));
+        bool set_run_mode = false;
 
         if (changes.Has(DiscoBallFunction::kRunning))
         {
             ChipLogDetail(Zcl, "  --> Running = %s", (cluster.GetRunAttribute() ? "true" : "false"));
+            set_run_mode = true;
         }
 
         if (changes.Has(DiscoBallFunction::kRotation))
         {
             ChipLogDetail(Zcl, "  --> Rotate = %u", static_cast<unsigned>(cluster.GetRotateAttribute()));
+            set_run_mode = true;
         }
 
         if (changes.Has(DiscoBallFunction::kSpeed))
         {
             ChipLogDetail(Zcl, "  --> Speed = %u", static_cast<unsigned>(cluster.GetSpeedAttribute()));
+            set_run_mode = true;
         }
 
         if (changes.Has(DiscoBallFunction::kAxis))
@@ -117,6 +131,9 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
             CharSpan name = cluster.GetNameAttribute();
             ChipLogDetail(Zcl, "  --> Name = '%.*s'", static_cast<int>(name.size()), name.data());
         }
+        if (set_run_mode) {
+            SetRunMode(cluster);
+        }
 
         return Status::Success;
     }
@@ -134,6 +151,72 @@ class SampleDiscoBallDriver : public DiscoBallClusterLogic::DriverInterface
     void MarkAttributeDirty(const ConcreteAttributePath& path) override
     {
         MatterReportingAttributeChangeCallback(path);
+    }
+    void Init() {
+#if PI_DISCO_BALL
+        mPigpio = pigpio_start(nullptr, nullptr);
+        if (mPigpio < 0) {
+            ChipLogError(Zcl, "Unable to start pigpio");
+            return;
+        }
+        if (set_mode(mPigpio, mGpioForward, PI_PUD_DOWN) != 0 || set_mode(mPigpio, mGpioReverse, PI_PUD_DOWN) != 0) {
+            ChipLogError(Zcl, "Unable to set output mode");
+            ShutdownGpio();
+            return;
+        }
+        if (set_pull_up_down(mPigpio, mGpioForward, PI_PUD_DOWN) != 0 || set_pull_up_down(mPigpio, mGpioReverse, PI_PUD_DOWN) != 0) {
+            ChipLogError(Zcl, "Unable to set pulldown mode");
+            ShutdownGpio();
+            return;
+        }
+        ChipLogProgress(Zcl, "Set up Raspi pin PWM on %d---------------------------------------", mPigpio);
+#endif
+    }
+    void ShutdownGpio() {
+        if (mPigpio >= 0) {
+            pigpio_stop(mPigpio);
+            mPigpio = -1;
+        }
+    }
+    void Shutdown() {
+        ShutdownGpio();
+    }
+private:
+    int mPigpio = -1;
+    int mGpioForward = 20;
+    int mGpioReverse = 26;
+    CHIP_ERROR SetRunMode(DiscoBallClusterLogic & cluster) {
+#if PI_DISCO_BALL
+	int gpio_err_forward = set_PWM_dutycycle(mPigpio, mGpioForward, 0);
+        int gpio_err_reverse = set_PWM_dutycycle(mPigpio, mGpioReverse, 0);
+        if (gpio_err_forward != 0 || gpio_err_reverse != 0) {
+            ChipLogError(Zcl, "Error setting PWM duty cycle on GPIO pins. Unable to stop disco ball. err forward = %s err reverse = %s", pigpio_error(gpio_err_forward), pigpio_error(gpio_err_reverse));
+            return CHIP_ERROR_INCORRECT_STATE;
+        }
+        if (cluster.GetRunAttribute()) {
+            // Duty cycle of less than 100 doens't work for the current motor, so use that as a minimum
+            // Duty cycle range is therefore 100-200, motor range is 0-200, so just map the range as appropriate
+            // Note that this has no bearing on reality becuase that motor is spinning like the dickens
+            int speed = (cluster.GetSpeedAttribute() >> 1) + 100;
+            switch(cluster.GetRotateAttribute()) {
+            case Clusters::DiscoBall::RotateEnum::kClockwise:
+                gpio_err_forward = set_PWM_dutycycle(mPigpio, mGpioForward, speed);
+                if (gpio_err_forward != 0) {
+                    ChipLogError(Zcl, "Unable to start GPIO PWM: %s", pigpio_error(gpio_err_forward));
+                }
+		break;
+            case Clusters::DiscoBall::RotateEnum::kCounterClockwise:
+                gpio_err_reverse = set_PWM_dutycycle(mPigpio, mGpioReverse, speed);
+                if (gpio_err_reverse != 0) {
+                    ChipLogError(Zcl, "Unable to start GPIO PWM: %s", pigpio_error(gpio_err_reverse));
+                }
+                break;
+            default:
+                return CHIP_ERROR_INCORRECT_STATE;
+            }
+        }
+#endif
+        return CHIP_NO_ERROR;
     }
 };
 
