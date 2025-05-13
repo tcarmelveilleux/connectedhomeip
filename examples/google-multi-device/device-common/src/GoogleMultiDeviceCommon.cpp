456197a3858ee65ff93c3c9532ad71fb50d1ea6c
tennessee.carmelveilleux@gmail.com <tennessee.carmelveilleux@gmail.com>
2025-05-13 10:14:50 -0400

    enable of clusters

--- examples/google-multi-device/device-common/src/GoogleMultiDeviceCommon.cpp
+++ examples/google-multi-device/device-common/src/GoogleMultiDeviceCommon.cpp
@@ -199,11 +199,11 @@ void GoogleMultiDeviceIntegration::InitializeProduct()
     mOpStateInstanceEp6->Init();
 
     // ============= Set application endpoints enables =============
-    emberAfEndpointEnableDisable(2, false);
-    emberAfEndpointEnableDisable(3, false);
-    emberAfEndpointEnableDisable(4, false);
-    emberAfEndpointEnableDisable(5, true);
-    emberAfEndpointEnableDisable(6, false);
+    // emberAfEndpointEnableDisable(2, false);
+    // emberAfEndpointEnableDisable(3, false);
+    // emberAfEndpointEnableDisable(4, false);
+    // emberAfEndpointEnableDisable(5, true);
+    // emberAfEndpointEnableDisable(6, false);
 }
 
 void GoogleMultiDeviceIntegration::HandleButtonPress(ButtonId buttonId)
